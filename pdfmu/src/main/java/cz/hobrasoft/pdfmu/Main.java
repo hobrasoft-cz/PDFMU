0861bebc3e75469d30252420d88502e0b7d8fe3a
Filip Bártek <filip.bartek@hotmail.com>
2015-08-31 15:34:38 +0200

    Main: Remove trailing dot in main parser description

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/Main.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/Main.java
@@ -20,7 +20,7 @@ public class Main {
      */
     public static void main(String[] args) {
         ArgumentParser parser = ArgumentParsers.newArgumentParser("pdfmu")
-                .description("Manipulate a PDF file.")
+                .description("Manipulate a PDF file")
                 .defaultHelp(true);
         parser.addArgument("-of", "--output-format")
                 .choices("text", "json")

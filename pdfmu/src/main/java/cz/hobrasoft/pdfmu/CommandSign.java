8ff6638194bfedb772df54f6e51ed681520300c1
Filip Bártek <filip.bartek@hotmail.com>
2015-08-31 19:41:37 +0200

    CommandSign: Add parser arguments

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/CommandSign.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/CommandSign.java
@@ -1,5 +1,6 @@
 package cz.hobrasoft.pdfmu;
 
+import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.Subparser;
 import net.sourceforge.argparse4j.inf.Subparsers;
 
@@ -18,12 +19,36 @@ public class CommandSign implements Command {
     @Override
     public Subparser addParser(Subparsers subparsers) {
         String help = "Digitally sign a PDF document";
+        String metavarIn = "IN.pdf";
+        String metavarOut = "OUT.pdf";
+        String metavarCertificate = "CERTIFICATE";
         
         Subparser subparser = subparsers.addParser("sign")
                 .help(help)
                 .description(help)
                 .defaultHelp(true)
                 .setDefault("command", CommandSign.class);
+        subparser.addArgument("-i", "--in")
+                .help("input PDF file")
+                .type(String.class)
+                .required(true)
+                .nargs("?")
+                .metavar(metavarIn);
+        subparser.addArgument("-c", "--certificate")
+                .help("certificate file to sign with")
+                .type(String.class)
+                .required(true)
+                .nargs("?")
+                .metavar(metavarCertificate);
+        subparser.addArgument("-o", "--out")
+                .help("output PDF file")
+                .type(String.class)
+                .nargs("?")
+                .metavar(metavarOut);
+        subparser.addArgument("--force")
+                .help(String.format("overwrite %s if already present", metavarOut))
+                .type(boolean.class)
+                .action(Arguments.storeTrue());
         
         return subparser;
     }

2ba61a8934f84bd192828a33e537aab0250076b1
Filip Bártek <filip.bartek@hotmail.com>
2015-09-11 15:59:41 +0200

    OperationSign: Reorder the members: adore with comments, make methods static where appropriate
    The reorder and comments should improve readability a lot.
    Now the methods are listed in the order they are called in a normal use.
    Since they stack linearly, the order is easy to follow.

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/sign/OperationSign.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/sign/OperationSign.java
@@ -38,6 +38,47 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
  */
 public class OperationSign implements Operation {
 
+    @Override
+    public String getCommandName() {
+        return "sign";
+    }
+
+    @Override
+    public Subparser configureSubparser(Subparser subparser) {
+        String help = "Digitally sign a PDF document";
+
+        String metavarIn = "IN.pdf";
+        String metavarOut = "OUT.pdf";
+
+        // Configure the subparser
+        subparser.help(help)
+                .description(help)
+                .defaultHelp(true)
+                .setDefault("command", OperationSign.class);
+
+        // Add arguments to the subparser
+        // Positional arguments are required by default
+        subparser.addArgument("in")
+                .help("input PDF document")
+                .metavar(metavarIn)
+                .type(Arguments.fileType().acceptSystemIn().verifyCanRead())
+                .required(true);
+
+        subparser.addArgument("-o", "--out")
+                .help(String.format("output PDF document (default: <%s>)", metavarIn))
+                .metavar(metavarOut)
+                .type(Arguments.fileType().verifyCanCreate())
+                .nargs("?");
+        subparser.addArgument("-f", "--force")
+                .help(String.format("overwrite %s if it exists", metavarOut))
+                .type(boolean.class)
+                .action(Arguments.storeTrue());
+
+        signatureParameters.addArguments(subparser);
+
+        return subparser;
+    }
+
     // digitalsignatures20130304.pdf : Code sample 1.6
     // Initialize the security provider
     private static final BouncyCastleProvider provider = new BouncyCastleProvider();
@@ -58,114 +99,81 @@ public class OperationSign implements Operation {
     // because the interface `ArgsConfiguration` does not allow static methods.
     private final SignatureParameters signatureParameters = new SignatureParameters();
 
-    private static void sign(PdfSignatureAppearance sap,
-            ExternalDigest externalDigest,
-            ExternalSignature externalSignature,
-            Certificate[] chain,
-            MakeSignature.CryptoStandard sigtype) throws OperationException {
-        // TODO?: Set some of the following parameters more sensibly
+    @Override
+    public void execute(Namespace namespace) throws OperationException {
+        // Input file
+        File inFile = namespace.get("in");
+        assert inFile != null; // Required argument
+        System.err.println(String.format("Input PDF document: %s", inFile));
 
-        // Certificate Revocation List
-        // digitalsignatures20130304.pdf : Section 3.2
-        Collection<CrlClient> crlList = null;
-
-        // Online Certificate Status Protocol
-        // digitalsignatures20130304.pdf : Section 3.2.4
-        OcspClient ocspClient = null;
-
-        // Time Stamp Authority
-        // digitalsignatures20130304.pdf : Section 3.3
-        TSAClient tsaClient = null;
-
-        // digitalsignatures20130304.pdf : Section 3.5
-        // The value of 0 means "try a generous educated guess".
-        // We need not change this unless we want to optimize the resulting PDF document size.
-        int estimatedSize = 0;
-
-        System.err.println(String.format("Cryptographic standard (signature format): %s", sigtype));
-
-        try {
-            MakeSignature.signDetached(sap, externalDigest, externalSignature, chain, crlList, ocspClient, tsaClient, estimatedSize, sigtype);
-        } catch (IOException | DocumentException | GeneralSecurityException ex) {
-            throw new OperationException("Could not sign the document.", ex);
-        } catch (NullPointerException ex) {
-            throw new OperationException("Could not sign the document. Invalid digest algorithm?", ex);
+        // Output file
+        File outFile = namespace.get("out");
+        if (outFile == null) {
+            System.err.println("--out option not specified; assuming in-place version change");
+            outFile = inFile;
         }
-        System.err.println("Document successfully signed.");
-    }
-
-    private static void sign(PdfSignatureAppearance sap,
-            ExternalSignature externalSignature,
-            Certificate[] chain,
-            MakeSignature.CryptoStandard sigtype) throws OperationException {
-        // Use the static BouncyCastleDigest instance
-        sign(sap, OperationSign.externalDigest, externalSignature, chain, sigtype);
-    }
-
-    private void sign(PdfSignatureAppearance sap,
-            PrivateKey pk,
-            String digestAlgorithm,
-            Certificate[] chain,
-            MakeSignature.CryptoStandard sigtype) throws OperationException {
-        assert digestAlgorithm != null;
-
-        // Initialize the signature algorithm
-        System.err.println(String.format("Digest algorithm: %s", digestAlgorithm));
-        if (DigestAlgorithms.getAllowedDigests(digestAlgorithm) == null) {
-            throw new OperationException(String.format("The digest algorithm %s is not supported.", digestAlgorithm));
+        System.err.println(String.format("Output PDF document: %s", outFile));
+        if (outFile.exists()) {
+            System.err.println("Output file already exists.");
+            if (!namespace.getBoolean("force")) {
+                throw new OperationException("Set --force flag to overwrite.");
+            }
         }
 
-        System.err.println(String.format("Signature security provider: %s", provider.getName()));
-        ExternalSignature externalSignature = new PrivateKeySignature(pk, digestAlgorithm, provider.getName());
+        boolean append = true; // TODO?: Expose
 
-        sign(sap, externalSignature, chain, sigtype);
+        // Initialize signature parameters
+        signatureParameters.setFromNamespace(namespace);
+
+        sign(inFile, outFile, append, signatureParameters);
     }
 
-    private void sign(PdfSignatureAppearance sap,
-            KeyStore ks,
-            KeyParameters keyParameters,
-            String digestAlgorithm,
-            MakeSignature.CryptoStandard sigtype) throws OperationException {
-        assert keyParameters != null;
-        // Fix the values, especially if they were not set at all
-        keyParameters.fix(ks);
-
-        PrivateKey pk = keyParameters.getPrivateKey(ks);
-        Certificate[] chain = keyParameters.getCertificateChain(ks);
-
-        sign(sap, pk, digestAlgorithm, chain, sigtype);
-    }
-
-    private void sign(PdfSignatureAppearance sap,
-            KeystoreParameters keystoreParameters,
-            KeyParameters keyParameters,
-            String digestAlgorithm,
-            MakeSignature.CryptoStandard sigtype) throws OperationException {
-        assert keystoreParameters != null;
-
-        // Initialize and load keystore
-        KeyStore ks = keystoreParameters.loadKeystore();
-
-        sign(sap, ks, keyParameters, digestAlgorithm, sigtype);
-    }
-
-    private void sign(PdfStamper stp,
+    // Open the PDF reader
+    private static void sign(File inFile,
+            File outFile,
+            boolean append,
             SignatureParameters signatureParameters) throws OperationException {
-        assert signatureParameters != null;
-        // Unwrap the signature parameters
-        SignatureAppearanceParameters signatureAppearanceParameters = signatureParameters.appearance;
-        KeystoreParameters keystoreParameters = signatureParameters.keystore;
-        KeyParameters keyParameters = signatureParameters.key;
-        String digestAlgorithm = signatureParameters.digestAlgorithm;
-        MakeSignature.CryptoStandard sigtype = signatureParameters.sigtype;
+        assert inFile != null;
 
-        // Initialize the signature appearance
-        PdfSignatureAppearance sap = signatureAppearanceParameters.getSignatureAppearance(stp);
+        System.err.println(String.format("Input PDF document: %s", inFile));
 
-        sign(sap, keystoreParameters, keyParameters, digestAlgorithm, sigtype);
+        // Open the input stream
+        FileInputStream inStream;
+        try {
+            inStream = new FileInputStream(inFile);
+        } catch (FileNotFoundException ex) {
+            throw new OperationException("Input file not found.", ex);
+        }
+
+        // Open the PDF reader
+        // PdfReader parses a PDF document.
+        PdfReader pdfReader;
+        try {
+            pdfReader = new PdfReader(inStream);
+        } catch (IOException ex) {
+            throw new OperationException("Could not open the input PDF document.", ex);
+        }
+
+        if (outFile == null) {
+            System.err.println("Output file not set. Commencing in-place operation.");
+            outFile = inFile;
+        }
+
+        sign(pdfReader, outFile, append, signatureParameters);
+
+        // Close the PDF reader
+        pdfReader.close();
+
+        // Close the input stream
+        try {
+            inStream.close();
+        } catch (IOException ex) {
+            throw new OperationException("Could not close the input file.", ex);
+        }
     }
 
-    private void sign(PdfReader pdfReader,
+    // Open the PDF stamper
+    private static void sign(PdfReader pdfReader,
             File outFile,
             boolean append,
             SignatureParameters signatureParameters) throws OperationException {
@@ -213,117 +221,117 @@ public class OperationSign implements Operation {
         }
     }
 
-    private void sign(File inFile,
-            File outFile,
-            boolean append,
+    // Initialize the signature appearance
+    private static void sign(PdfStamper stp,
             SignatureParameters signatureParameters) throws OperationException {
-        assert inFile != null;
+        assert signatureParameters != null;
+        // Unwrap the signature parameters
+        SignatureAppearanceParameters signatureAppearanceParameters = signatureParameters.appearance;
+        KeystoreParameters keystoreParameters = signatureParameters.keystore;
+        KeyParameters keyParameters = signatureParameters.key;
+        String digestAlgorithm = signatureParameters.digestAlgorithm;
+        MakeSignature.CryptoStandard sigtype = signatureParameters.sigtype;
 
-        System.err.println(String.format("Input PDF document: %s", inFile));
+        // Initialize the signature appearance
+        PdfSignatureAppearance sap = signatureAppearanceParameters.getSignatureAppearance(stp);
 
-        // Open the input stream
-        FileInputStream inStream;
-        try {
-            inStream = new FileInputStream(inFile);
-        } catch (FileNotFoundException ex) {
-            throw new OperationException("Input file not found.", ex);
-        }
-
-        // Open the PDF reader
-        // PdfReader parses a PDF document.
-        PdfReader pdfReader;
-        try {
-            pdfReader = new PdfReader(inStream);
-        } catch (IOException ex) {
-            throw new OperationException("Could not open the input PDF document.", ex);
-        }
-
-        if (outFile == null) {
-            System.err.println("Output file not set. Commencing in-place operation.");
-            outFile = inFile;
-        }
-
-        sign(pdfReader, outFile, append, signatureParameters);
-
-        // Close the PDF reader
-        pdfReader.close();
-
-        // Close the input stream
-        try {
-            inStream.close();
-        } catch (IOException ex) {
-            throw new OperationException("Could not close the input file.", ex);
-        }
+        sign(sap, keystoreParameters, keyParameters, digestAlgorithm, sigtype);
     }
 
-    @Override
-    public void execute(Namespace namespace) throws OperationException {
-        // Input file
-        File inFile = namespace.get("in");
-        assert inFile != null; // Required argument
-        System.err.println(String.format("Input PDF document: %s", inFile));
+    // Initialize and load the keystore
+    private static void sign(PdfSignatureAppearance sap,
+            KeystoreParameters keystoreParameters,
+            KeyParameters keyParameters,
+            String digestAlgorithm,
+            MakeSignature.CryptoStandard sigtype) throws OperationException {
+        assert keystoreParameters != null;
 
-        // Output file
-        File outFile = namespace.get("out");
-        if (outFile == null) {
-            System.err.println("--out option not specified; assuming in-place version change");
-            outFile = inFile;
-        }
-        System.err.println(String.format("Output PDF document: %s", outFile));
-        if (outFile.exists()) {
-            System.err.println("Output file already exists.");
-            if (!namespace.getBoolean("force")) {
-                throw new OperationException("Set --force flag to overwrite.");
-            }
-        }
+        // Initialize and load keystore
+        KeyStore ks = keystoreParameters.loadKeystore();
 
-        boolean append = true; // TODO?: Expose
-
-        // Initialize signature parameters
-        signatureParameters.setFromNamespace(namespace);
-
-        sign(inFile, outFile, append, signatureParameters);
+        sign(sap, ks, keyParameters, digestAlgorithm, sigtype);
     }
 
-    @Override
-    public Subparser configureSubparser(Subparser subparser) {
-        String help = "Digitally sign a PDF document";
+    // Get the private key and the certificate chain from the keystore
+    private static void sign(PdfSignatureAppearance sap,
+            KeyStore ks,
+            KeyParameters keyParameters,
+            String digestAlgorithm,
+            MakeSignature.CryptoStandard sigtype) throws OperationException {
+        assert keyParameters != null;
+        // Fix the values, especially if they were not set at all
+        keyParameters.fix(ks);
 
-        String metavarIn = "IN.pdf";
-        String metavarOut = "OUT.pdf";
+        PrivateKey pk = keyParameters.getPrivateKey(ks);
+        Certificate[] chain = keyParameters.getCertificateChain(ks);
 
-        // Configure the subparser
-        subparser.help(help)
-                .description(help)
-                .defaultHelp(true)
-                .setDefault("command", OperationSign.class);
-
-        // Add arguments to the subparser
-        // Positional arguments are required by default
-        subparser.addArgument("in")
-                .help("input PDF document")
-                .metavar(metavarIn)
-                .type(Arguments.fileType().acceptSystemIn().verifyCanRead())
-                .required(true);
-
-        subparser.addArgument("-o", "--out")
-                .help(String.format("output PDF document (default: <%s>)", metavarIn))
-                .metavar(metavarOut)
-                .type(Arguments.fileType().verifyCanCreate())
-                .nargs("?");
-        subparser.addArgument("-f", "--force")
-                .help(String.format("overwrite %s if it exists", metavarOut))
-                .type(boolean.class)
-                .action(Arguments.storeTrue());
-
-        signatureParameters.addArguments(subparser);
-
-        return subparser;
+        sign(sap, pk, digestAlgorithm, chain, sigtype);
     }
 
-    @Override
-    public String getCommandName() {
-        return "sign";
+    // Initialize the signature algorithm
+    private static void sign(PdfSignatureAppearance sap,
+            PrivateKey pk,
+            String digestAlgorithm,
+            Certificate[] chain,
+            MakeSignature.CryptoStandard sigtype) throws OperationException {
+        assert digestAlgorithm != null;
+
+        // Initialize the signature algorithm
+        System.err.println(String.format("Digest algorithm: %s", digestAlgorithm));
+        if (DigestAlgorithms.getAllowedDigests(digestAlgorithm) == null) {
+            throw new OperationException(String.format("The digest algorithm %s is not supported.", digestAlgorithm));
+        }
+
+        System.err.println(String.format("Signature security provider: %s", provider.getName()));
+        ExternalSignature externalSignature = new PrivateKeySignature(pk, digestAlgorithm, provider.getName());
+
+        sign(sap, externalSignature, chain, sigtype);
+    }
+
+    // Set the "external digest" algorithm
+    private static void sign(PdfSignatureAppearance sap,
+            ExternalSignature externalSignature,
+            Certificate[] chain,
+            MakeSignature.CryptoStandard sigtype) throws OperationException {
+        // Use the static BouncyCastleDigest instance
+        sign(sap, OperationSign.externalDigest, externalSignature, chain, sigtype);
+    }
+
+    // Sign the document
+    private static void sign(PdfSignatureAppearance sap,
+            ExternalDigest externalDigest,
+            ExternalSignature externalSignature,
+            Certificate[] chain,
+            MakeSignature.CryptoStandard sigtype) throws OperationException {
+        // TODO?: Set some of the following parameters more sensibly
+
+        // Certificate Revocation List
+        // digitalsignatures20130304.pdf : Section 3.2
+        Collection<CrlClient> crlList = null;
+
+        // Online Certificate Status Protocol
+        // digitalsignatures20130304.pdf : Section 3.2.4
+        OcspClient ocspClient = null;
+
+        // Time Stamp Authority
+        // digitalsignatures20130304.pdf : Section 3.3
+        TSAClient tsaClient = null;
+
+        // digitalsignatures20130304.pdf : Section 3.5
+        // The value of 0 means "try a generous educated guess".
+        // We need not change this unless we want to optimize the resulting PDF document size.
+        int estimatedSize = 0;
+
+        System.err.println(String.format("Cryptographic standard (signature format): %s", sigtype));
+
+        try {
+            MakeSignature.signDetached(sap, externalDigest, externalSignature, chain, crlList, ocspClient, tsaClient, estimatedSize, sigtype);
+        } catch (IOException | DocumentException | GeneralSecurityException ex) {
+            throw new OperationException("Could not sign the document.", ex);
+        } catch (NullPointerException ex) {
+            throw new OperationException("Could not sign the document. Invalid digest algorithm?", ex);
+        }
+        System.err.println("Document successfully signed.");
     }
 
 }

f79867dd5ca5f84ea7a3650658085324a201ec28
Filip Bártek <filip.bartek@hotmail.com>
2015-09-11 15:43:27 +0200

    PdfVersion: Reformat

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/version/PdfVersion.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/version/PdfVersion.java
@@ -8,12 +8,13 @@ import java.util.regex.Pattern;
  *
  * <p>
  * Versions 1.2 to 1.7 are supported. This range corresponds to the versions
- * supported by iText, as seen in {@link com.itextpdf.text.pdf.PdfWriter}
- * static fields {@link com.itextpdf.text.pdf.PdfWriter#VERSION_1_2} etc.
+ * supported by iText, as seen in {@link com.itextpdf.text.pdf.PdfWriter} static
+ * fields {@link com.itextpdf.text.pdf.PdfWriter#VERSION_1_2} etc.
  *
  * @author <a href="mailto:filip.bartek@hobrasoft.cz">Filip Bartek</a>
  */
 public class PdfVersion implements Comparable<PdfVersion> {
+
     private static Pattern p = Pattern.compile("1\\.(?<charValue>[2-7])");
     private char charValue;
 
@@ -21,8 +22,7 @@ public class PdfVersion implements Comparable<PdfVersion> {
      * Creates a PDF version from its string representation
      *
      * <p>
-     * Versions 1.2 to 1.7 are supported. Valid string representations
-     * include:
+     * Versions 1.2 to 1.7 are supported. Valid string representations include:
      * <ul>
      * <li>{@code 1.3}</li>
      * <li>{@code 1.6}</li>
@@ -47,16 +47,16 @@ public class PdfVersion implements Comparable<PdfVersion> {
      * representation
      *
      * <p>
-     * Since versions 1.2 to 1.7 are supported, a valid PDF version is
-     * uniquely specified by its last character (the digit 2 to 7).
+     * Since versions 1.2 to 1.7 are supported, a valid PDF version is uniquely
+     * specified by its last character (the digit 2 to 7).
      *
      * <p>
      * Version in this format is returned by
      * {@link com.itextpdf.text.pdf.PdfReader#getPdfVersion()}.
      *
      * @param charValue last character of the PDF version
-     * @throws IllegalArgumentException if charValue does not represent a
-     * valid version
+     * @throws IllegalArgumentException if charValue does not represent a valid
+     * version
      */
     public PdfVersion(char charValue) throws IllegalArgumentException {
         if (charValue < '2' || charValue > '7') {
@@ -87,5 +87,5 @@ public class PdfVersion implements Comparable<PdfVersion> {
     public int compareTo(PdfVersion o) {
         return charValue - o.charValue;
     }
-    
+
 }

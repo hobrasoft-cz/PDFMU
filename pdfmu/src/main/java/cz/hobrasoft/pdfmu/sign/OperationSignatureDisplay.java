b92879aca0a2ee9254cf2a31c099f213b17e2436
Filip Bártek <filip.bartek@hotmail.com>
2015-09-12 16:55:47 +0200

    OperationSignatureDisplay: Refactor: Introduce an intermediate method `verifySignature`

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/sign/OperationSignatureDisplay.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/sign/OperationSignatureDisplay.java
@@ -125,11 +125,19 @@ public class OperationSignatureDisplay implements Operation {
         // digitalsignatures20130304.pdf : Code sample 5.2
         System.err.println(String.format("  Signature covers the whole document: %b", fields.signatureCoversWholeDocument(name)));
         System.err.println(String.format("  Document revision: %d of %d", fields.getRevision(name), fields.getTotalRevisions()));
+
         PdfPKCS7 pkcs7 = fields.verifySignature(name);
+        verifySignature(pkcs7);
+
+        return pkcs7;
+    }
+
+    private static PdfPKCS7 verifySignature(PdfPKCS7 pkcs7) throws OperationException {
+        // digitalsignatures20130304.pdf : Code sample 5.2
         try {
             System.err.println(String.format("  Integrity check OK?: %b", pkcs7.verify()));
         } catch (GeneralSecurityException ex) {
-            throw new OperationException(String.format("Could not verify signature %s.", name), ex);
+            throw new OperationException("Could not verify a signature.", ex);
         }
         return pkcs7;
     }

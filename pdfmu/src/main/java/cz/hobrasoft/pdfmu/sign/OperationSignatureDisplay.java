ef30f26d82b53ca203c121373c4a18aa91119186
Filip Bártek <filip.bartek@hotmail.com>
2015-09-12 16:50:42 +0200

    OperationSignatureDisplay: Verify signatures
    Use `PdfPKCS7.verify()` to verify the signatures.
    Also display revision numbers and whether the signature covers the whole document.
    Copied from Code sample 5.2 of the signature book.

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/sign/OperationSignatureDisplay.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/sign/OperationSignatureDisplay.java
@@ -2,6 +2,7 @@ package cz.hobrasoft.pdfmu.sign;
 
 import com.itextpdf.text.pdf.AcroFields;
 import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.security.PdfPKCS7;
 import cz.hobrasoft.pdfmu.Operation;
 import cz.hobrasoft.pdfmu.OperationException;
 import java.io.File;
@@ -9,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 import net.sourceforge.argparse4j.impl.Arguments;
 import net.sourceforge.argparse4j.inf.Namespace;
@@ -98,12 +100,38 @@ public class OperationSignatureDisplay implements Operation {
         pdfReader.close();
     }
 
-    private static void display(PdfReader pdfReader) {
+    private static void display(PdfReader pdfReader) throws OperationException {
+        // digitalsignatures20130304.pdf : Code sample 5.1
         AcroFields fields = pdfReader.getAcroFields();
+        display(fields);
+    }
+
+    private static void display(AcroFields fields) throws OperationException {
+        // digitalsignatures20130304.pdf : Code sample 5.1
         ArrayList<String> names = fields.getSignatureNames();
+
+        // Print number of signatures
+        System.err.println(String.format("Number of signatures: %d", names.size()));
+        System.err.println(String.format("Number of document revisions: %d", fields.getTotalRevisions()));
+
         for (String name : names) {
-            System.err.println(String.format("Signature name: %s", name));
+            System.err.println(); // Separate singatures by an empty line
+            System.err.println(String.format("Signature field name: %s", name));
+            verifySignature(fields, name);
         }
+    }
+
+    private static PdfPKCS7 verifySignature(AcroFields fields, String name) throws OperationException {
+        // digitalsignatures20130304.pdf : Code sample 5.2
+        System.err.println(String.format("  Signature covers the whole document: %b", fields.signatureCoversWholeDocument(name)));
+        System.err.println(String.format("  Document revision: %d of %d", fields.getRevision(name), fields.getTotalRevisions()));
+        PdfPKCS7 pkcs7 = fields.verifySignature(name);
+        try {
+            System.err.println(String.format("  Integrity check OK?: %b", pkcs7.verify()));
+        } catch (GeneralSecurityException ex) {
+            throw new OperationException(String.format("Could not verify signature %s.", name), ex);
+        }
+        return pkcs7;
     }
 
 }

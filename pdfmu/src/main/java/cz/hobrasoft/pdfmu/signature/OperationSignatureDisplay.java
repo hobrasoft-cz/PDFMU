0a4d46fd6f552bf96a0f7be27f3f5cce01ea5a2a
Filip Bártek <filip.bartek@hotmail.com>
2015-09-15 12:36:23 +0200

    OperationSignatureDisplay: Remove unused import

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/signature/OperationSignatureDisplay.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/signature/OperationSignatureDisplay.java
@@ -13,7 +13,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.GeneralSecurityException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.HashMap;

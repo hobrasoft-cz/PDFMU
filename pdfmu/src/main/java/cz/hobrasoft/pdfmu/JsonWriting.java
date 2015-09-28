3dbbcdce1aeb07abada95599efa8d6279cd01c4f
Filip Bártek <filip.bartek@hotmail.com>
2015-09-28 16:03:29 +0200

    Make a JSON writer available in every operation
    Use the field `json`.

---
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/JsonWriting.java
@@ -0,0 +1,6 @@
+package cz.hobrasoft.pdfmu;
+
+public interface JsonWriting {
+
+    public void setJsonWriter(JSONWriterEx json);
+}
--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/operation/OperationCommon.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/operation/OperationCommon.java
@@ -1,4 +1,14 @@
 package cz.hobrasoft.pdfmu.operation;
 
-public abstract class OperationCommon implements Operation {
+import cz.hobrasoft.pdfmu.JSONWriterEx;
+import cz.hobrasoft.pdfmu.JsonWriting;
+
+public abstract class OperationCommon implements Operation, JsonWriting {
+
+    private JSONWriterEx json;
+
+    @Override
+    public void setJsonWriter(JSONWriterEx json) {
+        this.json = json;
+    }
 }

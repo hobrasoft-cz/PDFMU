f7972bcad4908f2978ecf0bf02f76d819d648f0d
Filip Bártek <filip.bartek@hotmail.com>
2015-10-05 03:00:41 +0200

    Revert "Make a JSON writer available in every operation"
    This reverts commit 3dbbcdce1aeb07abada95599efa8d6279cd01c4f.

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/JsonWriting.java
+++
@@ -1,6 +0,0 @@
-package cz.hobrasoft.pdfmu;
-
-public interface JsonWriting {
-
-    public void setJsonWriter(JSONWriterEx json);
-}
--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/operation/OperationCommon.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/operation/OperationCommon.java
@@ -1,14 +1,4 @@
 package cz.hobrasoft.pdfmu.operation;
 
-import cz.hobrasoft.pdfmu.JSONWriterEx;
-import cz.hobrasoft.pdfmu.JsonWriting;
-
-public abstract class OperationCommon implements Operation, JsonWriting {
-
-    private JSONWriterEx json;
-
-    @Override
-    public void setJsonWriter(JSONWriterEx json) {
-        this.json = json;
-    }
+public abstract class OperationCommon implements Operation {
 }

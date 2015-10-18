e15b969823b7d33302ee0d49de372387b44b9d8f
Filip Bártek <filip.bartek@hotmail.com>
2015-10-18 19:18:55 +0200

    WritingMapper: Add documentation

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/WritingMapper.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/WritingMapper.java
@@ -5,6 +5,11 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 /**
+ * An {@link ObjectMapper} that writes JSON documents to an {@link OutputStream}
+ *
+ * <p>
+ * This class is a simple wrapper for {@link ObjectMapper} and
+ * {@link OutputStream}.
  *
  * @author <a href="mailto:filip.bartek@hobrasoft.cz">Filip Bartek</a>
  */
@@ -13,11 +18,26 @@ public class WritingMapper {
     private final ObjectMapper mapper;
     private final OutputStream os;
 
+    /**
+     * Creates a {@link WritingMapper}
+     *
+     * @param mapper the {@link ObjectMapper} to use for writing JSON documents
+     * @param os the target {@link OutputStream}
+     */
     public WritingMapper(ObjectMapper mapper, OutputStream os) {
         this.mapper = mapper;
         this.os = os;
     }
 
+    /**
+     * Serializes a Java value as a JSON output, streaming it to the specified
+     * {@link OutputStream}
+     *
+     * @param value the Java value to be serialized
+     * @throws IOException if the underlying
+     * {@link ObjectMapper#writeValue(OutputStream, Object)} throws an
+     * {@link IOException}
+     */
     public void writeValue(Object value) throws IOException {
         mapper.writeValue(os, value);
     }

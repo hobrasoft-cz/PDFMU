210f7854584e7a97f08030a07fa3bc2912351c91
Filip Bártek <filip.bartek@hotmail.com>
2015-11-03 13:39:44 +0100

    EmptyResult: Output an empty object as `result` in JSON output where appropriate
    Previously the `result` object contained the single field `success: true`.

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/jackson/EmptyResult.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/jackson/EmptyResult.java
@@ -1,10 +1,13 @@
 package cz.hobrasoft.pdfmu.jackson;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
 /**
  *
  * @author <a href="mailto:filip.bartek@hobrasoft.cz">Filip Bartek</a>
  */
+// HACK: Adding a Jackson annotation to a class with no fields makes it parsable.
+// Source: http://stackoverflow.com/a/21319219/4054250
+@JsonAutoDetect(fieldVisibility=JsonAutoDetect.Visibility.NONE)
 public class EmptyResult extends Result {
-
-    public boolean success = true;
 }

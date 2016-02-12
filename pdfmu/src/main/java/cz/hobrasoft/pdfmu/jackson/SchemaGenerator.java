6f20586a56dc6415ee254aec4be952a8e8253e70
Filip Bártek <filip.bartek@hotmail.com>
2016-02-12 14:04:54 +0100

    REFACTOR: Remove unused classes

--- pdfmu/src/main/java/cz/hobrasoft/pdfmu/jackson/SchemaGenerator.java
+++ pdfmu/src/main/java/cz/hobrasoft/pdfmu/jackson/SchemaGenerator.java
@@ -41,11 +41,9 @@ public class SchemaGenerator {
 
         Map<String, Type> types = new HashMap<>();
         types.put("RpcResponse", RpcResponse.class);
-        types.put("result/version get", VersionGet.class);
         types.put("result/version set", VersionSet.class);
         types.put("result/signature show", SignatureDisplay.class);
         types.put("result/signature add", SignatureAdd.class);
-        types.put("result/metadata get", MetadataGet.class);
         types.put("result/empty", EmptyResult.class);
 
         for (Map.Entry<String, Type> e : types.entrySet()) {
